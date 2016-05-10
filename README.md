9a6476ebe36750c30949786814578d4e39de0fbf
lara_m <merwan.lara@epitech.eu>
2016-05-10 11:51:17 +0200

    Little changes in quizz.h, bdd becomes dtb for English comprehension

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# quizzo
+# Quizzo
+Quizzo is a friendly bot who allows you to setup Quizz games within your guild.
+
+This still is a WIP.
--- include/quizz.h
+++ include/quizz.h
@@ -5,7 +5,7 @@
 ** Login   <player@epitech.net>
 **
 ** Started on  Mon May  9 18:21:55 2016 Merwan
-** Last update Mon May  9 20:53:29 2016 Merwan
+** Last update Tue May 10 11:48:20 2016 Merwan Lara
 */
 
 #ifndef _QUIZZ_H_
@@ -20,12 +20,12 @@
  /*
  ** ERROR MESSAGES
  */
-# define ERR_OPEN_FAILED_EN "Error : bdd-en couldn't be opened.\n"
-# define ERR_OPEN_FAILED_FR "Erreur : bdd-fr n'a pas pû être ouvert.\n"
+# define ERR_OPEN_FAILED_EN "Error : dtb-en.txt couldn't be opened.\n"
+# define ERR_OPEN_FAILED_FR "Erreur : dtb-fr.txt n'a pas pû être ouvert.\n"
 
  /*
  ** MODES
- ** See bdd-en.txt for more details
+ ** See dtb-en.txt for more details
  */
 # define SA 1 /* Single Answer Question */
 # define MCQ 2 /* Multiple Choice Question */
@@ -44,8 +44,8 @@
  ** RELATIVES PATHS FOR THE DATABASES AND THE HELP FILES
  ** TODO: Make them not relative
  */
-# define BDD_EN_PATH "database/dtb-en.txt"
-# define BDD_FR_PATH "database/dtb-fr.txt"
+# define BDD_EN_PATH "databases/dtb-en.txt"
+# define BDD_FR_PATH "databases/dtb-fr.txt"
 # define HELP_EN_PATH "help/help-en.txt"
 # define HELP_FR_PATH "help/help-fr.txt"
 
